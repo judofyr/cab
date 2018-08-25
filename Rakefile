5c979afab657a6125f7f5c79f830a7d37bae7c47
Magnus Holm <judofyr@gmail.com>
2018-08-25 13:58:27 +0200

    Add a more complicated test case

--- Rakefile
+++ Rakefile
@@ -9,6 +9,7 @@ task :default => [:test]
 
 task :test do
   sh 'ruby', 'test/run1.rb'
+  sh 'ruby', 'test/run2.rb'
   puts "*** All tests passed"
 end
 
---
+++ test/run2.rb
@@ -0,0 +1,22 @@
+require_relative 'support'
+require 'cab'
+
+GitDirectory.with_mbox(File.expand_path('example2.mbox', __dir__)) do |walker|
+  cab = Cab.new(dir: walker.path.to_s) do
+    require(walker.path + 'main.rb')
+    ::A::Value
+  end
+
+  walker.next
+  assert_equal 1, cab.run
+
+  walker.next
+  assert_equal 2, cab.run
+
+  walker.next
+  assert_equal 3, cab.run
+
+  walker.next
+  assert_equal 3, cab.run
+end
+
